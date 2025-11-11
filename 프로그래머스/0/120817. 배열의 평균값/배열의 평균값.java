90ee0c8d244e0d2a864e3ae353db5ef81448818e
taeho Jeong <t2222h@dgu.ac.kr>
2025-11-12 06:00:51 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,14 +1,12 @@
 class Solution {
     public double solution(int[] numbers) {
         double answer = 0;
-        int count = 0;
-        
+    
         for(int number : numbers) {
             answer += (double)number;
-            count++;
         }
         
-        answer = answer/count;
+        answer = answer/numbers.length;
         
         return answer;
     }
